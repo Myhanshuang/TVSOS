f0a5db33285972d5848c525bcaedd3d7e813ed29
kirito <3322640054@qq.com>
2025-09-08 16:27:39 +0800

    init project

---
+++ tvsos-backend/routing/routing-server/src/test/java/com/tvsos/RoutingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvsos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
